0ce4e3d0bd8854cca6df61e9e5733c01f5b7ac68
ketchupbbang <lastcodingdance@gmail.com>
2023-07-30 14:13:03 +0900

    충돌 판정 Ball 클래스로 다시 옮김 + 트리플 볼 구현해놓음 + 점수 획득시, 공이 화면 중앙으로 옮겨지고 각도를 다시 설정하게 함

--- pong_item.py
+++ pong_item.py
@@ -16,32 +16,109 @@ class Ball:
         self.speed = 0
         self.max_speed = 7  # 공 최고속도
         self.color = 7
+        self.fake = False
         self.can_move = False  # 공의 움직임 여부
         self.get_item = False  # 공이 아이템에 닿았을 때 여부
-        self.player = 1  # 공을 친 사람을 구분하는 변수(아이템 사용자
+        self.player = 'player'  # 공을 친 사람을 구분하는 변수(아이템 사용자)
 
-    def initialize(self, x, y, r=5, color=7, speed=6, angle=-1):
+    def initialize(self, x, y, r=5, color=7, speed=10, angle=-1):
         self.x = x
         self.y = y
         self.r = r
         self.color = color
         self.speed = speed
 
-        angle = angle if angle >= 0 else random.randint(300, 420)
+        if not angle >= 0:
+            if self.player == 'player':
+                angle = random.randint(300, 420)
+            elif self.player == 'enemy':
+                angle = random.randint(120, 240)
+
         self.dx = math.cos(math.radians(angle))
         self.dy = math.sin(math.radians(angle))
         self.can_move = True
 
-    # 공이 아이템에 닿았을 때 실행
-    def get_item(self, player):
-        pass
+    def move(self):
+        if self.can_move:
+            if self.game.started:
+                # 공이 상하 벽에 충돌할 경우
+                if self.y - self.r < 0:
+                    self.dy = abs(self.dy)
+                if self.y + self.r > 360:
+                    self.dy = -abs(self.dy)
+                """ # 공이 오른쪽 경계에 충돌할 경우
+                if self.x + self.r > 480:
+                    self.dx = -abs(self.dx)
+                # 공이 왼쪽 경계에 충돌할 경우
+                if self.x - self.r < 0:
+                    self.dx = abs(self.dx) """
+                # 공이 왼쪽/오른쪽 경계에 충돌할 경우 포인트 획득
+                if self.x + self.r > 480 or self.x - self.r < 0:
+                    self.get_point()
+                    if not self.fake:
+                        self.ori_location()
+                        self.game.time.count = pyxel.frame_count
+                # 공이 플레이어 바에 충돌할 경우
+                if 45 < self.x + self.r < 60:
+                    if self.game.bar.y - self.game.bar.height - self.r < self.y < self.game.bar.y \
+                            + self.game.bar.height + self.r:
+                        # 공이 닿은 패드의 위치에 따라 튕기는 각도가 변함
+                        ratio = (self.y - (self.game.bar.y - self.game.bar.height)) / (2 * self.game.bar.height)
+                        angle = int(300 + 120 * ratio)
+                        self.dx = math.cos(math.radians(angle))
+                        self.dy = math.sin(math.radians(angle))
+                        self.dx = abs(self.dx)
+                        # 플레이어 변경, 아이템 생성 시도, 아이템 공 제거
+                        if not self.fake:
+                            self.game.item.create_item()
+                            self.change_player('player')
+                        else:
+                            self.game.balls.remove(self)
+                # 공이 적 바에 충돌할 경우
+                if 435 > self.x + self.r > 420:
+                    if self.game.enemy_bar.y - self.game.enemy_bar.height - self.r < self.y < self.game.enemy_bar.y \
+                            + self.game.enemy_bar.height + self.r:
+                        self.dx = -abs(self.dx)
+                        # 플레이어 변경, 아이템 생성 시도, 아이템 공 제거
+                        if not self.fake:
+                            self.game.item.create_item()
+                            self.change_player('enemy')
+                        else:
+                            self.game.balls.remove(self)
+                # 공 이동
+                self.x += self.dx * self.speed
+                self.y += self.dy * self.speed
+            else:
+                self.x = self.game.bar.x + self.game.bar.width + self.r + 5
+                self.y = self.game.bar.y
 
     #  누군가 득점을 했을 때 실행
-    def get_point(self, player):
-        if player == 1:
+    def get_point(self):
+        if self.player == 'player':
             self.game.player.score += 1
-        else:
+        elif self.player == 'enemy':
             self.game.enemy.score += 1
+
+        if self.fake:
+            self.game.balls.remove(self)
+
+    def ori_location(self):
+        self.x = 240
+        self.y = 180
+        angle = 0
+
+        if self.player == 'player':
+            angle = random.randint(120, 240)
+            self.change_player('enemy')
+        elif self.player == 'enemy':
+            angle = random.randint(300, 420)
+            self.change_player('player')
+
+        self.dx = math.cos(math.radians(angle))
+        self.dy = math.sin(math.radians(angle))
+
+    def change_player(self, player):
+        self.player = player
 
 
 class Bar:
@@ -69,9 +146,6 @@ class Bar:
         if self.y + self.height > 360:
             self.y = 360 - self.height
 
-    def collide(self):
-        pass
-
 
 class enemy_bar(Bar):
 
@@ -94,8 +168,37 @@ class Item:
 
     def __init__(self, game):
         self.game = game
+        self.x = 0
+        self.y = 0
+        self.r = 20
+        self.type = 0
+        self.color = 0
+        self.use_count = 0
+        self.existence = False
+        self.active = False
 
-        pass
+    def create_item(self):
+        if not self.existence:
+            # 아이템 등장 확률 설정
+            if random.randint(1, 1) == 1:
+                self.existence = True
+                # 아이템 좌표 랜덤 선택
+                self.x = random.randint(150, 330)
+                self.y = random.randint(50, 310)
+                # 아이템 종류 랜덤 선택
+                self.type = random.randint(1, 1)
+                if self.type == 1:
+                    self.color = 8
+
+    def detect_collision(self, ball: Ball):
+        if self.game.started:
+            if self.existence:
+                if not ball.fake:
+                    if (self.x - self.r - ball.r < ball.x < self.x + self.r + ball.r) and \
+                            (self.y - self.r - ball.r < ball.y < self.y + self.r + ball.r):
+                        self.active = True
+                        self.existence = False
+                        self.game.time.count = pyxel.frame_count
 
     def speed_ball(self):
         pass
@@ -103,8 +206,22 @@ class Item:
     def fake_ball(self):
         pass
 
-    def triple_ball(self):
-        pass
+    def triple_ball(self, ball: Ball):
+        ball.can_move = False
+        if (pyxel.frame_count - self.game.time.count) % 30 == 0:
+            if self.use_count == 3:
+                self.use_count = 0
+                ball.can_move = True
+                self.active = False
+            else:
+                self.use_count += 1
+                # 아이템 공 추가
+                new_ball = Ball(self.game)
+                if ball.player == 'enemy':
+                    new_ball.player = 'enemy'
+                new_ball.initialize(ball.x, ball.y, 5, 8, 10)
+                new_ball.fake = True
+                self.game.balls.append(new_ball)
 
     def yunepyoi(self):
         pass
@@ -117,6 +234,15 @@ class Player:
 
     def initialize(self):
         self.score = 0
+
+
+class Time:
+    def __init__(self, game):
+        self.game = game
+        self.count = 0
+
+    def count(self):
+        self.count += 1
 
 
 class Game:
@@ -136,13 +262,14 @@ class Game:
         self.bar.initialize(50, 201, 6, 20)
 
         self.enemy_bar = enemy_bar(self)
-        self.enemy_bar.initialize(430, 201, 6, 40, 3)
+        self.enemy_bar.initialize(430, 201, 6, 40, 5)
+
+        self.item = Item(self)
 
         self.balls = [Ball(self)]
         self.balls[0].initialize(250, self.bar.y, 5, 7, 10, -1)
-        new_ball = Ball(self)
-        new_ball.initialize(250, self.bar.y, 5, 7, 10, -1)
-        self.balls.append(new_ball)
+
+        self.time = Time(self)
 
         # 플레이어가 클릭하면 True로 변경
         self.started = False
@@ -161,62 +288,37 @@ class Game:
 
         self.bar.move()
         self.enemy_bar.move()
-        self.ball_move()
+        for ball in self.balls:
+            ball.move()
+            self.item.detect_collision(ball)
+            if self.item.active:
+                if self.item.type == 1:
+                    if not ball.fake:
+                        self.item.triple_ball(ball)
 
     def draw(self):
         pyxel.cls(0)
+
         pyxel.rect(self.bar.x - self.bar.width, self.bar.y - self.bar.height, self.bar.width * 2, self.bar.height * 2,
                    self.bar.color)
         pyxel.rect(self.enemy_bar.x - self.enemy_bar.width, self.enemy_bar.y - self.enemy_bar.height,
                    self.enemy_bar.width * 2,
                    self.enemy_bar.height * 2, self.enemy_bar.color)
+
         for ball in self.balls:
             pyxel.circ(ball.x, ball.y, ball.r, ball.color)
+
+        if self.item.existence:
+            pyxel.circ(self.item.x, self.item.y, self.item.r, self.item.color)
 
         pyxel.text(85, 315, f'{self.balls[0].x}', 7)
         pyxel.text(85, 335, f'{self.balls[0].y}', 7)
         pyxel.text(170, 315, f'{self.player.score}', 7)
         pyxel.text(170, 335, f'{self.enemy.score}', 7)
+        pyxel.text(240, 55, f'{pyxel.frame_count}', 7)
 
         if not self.started:
             pyxel.text(85, 215, 'Press SPACE to Start', 7)
-
-    def ball_move(self):
-        for ball in self.balls:
-            if ball.can_move:
-                if self.started:
-                    # 공이 상하 벽에 충돌할 경우
-                    if ball.y - ball.r < 0:
-                        ball.dy = abs(ball.dy)
-                    if ball.y + ball.r > 360:
-                        ball.dy = -abs(ball.dy)
-                    # 공이 오른쪽 경계에 충돌할 경우
-                    if ball.x + ball.r > 480:
-                        ball.dx = -abs(ball.dx)
-                        ball.get_point(1)
-                    # 공이 왼쪽 경계에 충돌할 경우
-                    if ball.x - ball.r < 0:
-                        ball.dx = abs(ball.dx)
-                        ball.get_point(2)
-                    # 공이 플레이어 바에 충돌할 경우
-                    if 45 < ball.x + ball.r < 60:
-                        if self.bar.y - self.bar.height - ball.r < ball.y < self.bar.y + self.bar.height + ball.r:
-                            # 공이 닿은 패드의 위치에 따라 튕기는 각도가 변함
-                            ratio = (ball.y - (self.bar.y - self.bar.height)) / (2 * self.bar.height)
-                            angle = int(300 + 120 * ratio)
-                            ball.dx = math.cos(math.radians(angle))
-                            ball.dy = math.sin(math.radians(angle))
-                            ball.dx = abs(ball.dx)
-                    # 공이 적 바에 충돌할 경우
-                    if 435 > ball.x + ball.r > 420:
-                        if self.enemy_bar.y - self.enemy_bar.height - ball.r < ball.y < self.enemy_bar.y + self.enemy_bar.height + ball.r:
-                            ball.dx = -abs(ball.dx)
-                    # 공 이동
-                    ball.x += ball.dx * ball.speed
-                    ball.y += ball.dy * ball.speed
-                else:
-                    ball.x = self.bar.x + self.bar.width + ball.r
-                    ball.y = self.bar.y
 
 
 Game()
